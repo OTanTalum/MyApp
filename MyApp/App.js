f9f0342f66e10d0b7ec15943f6053dadffc78155
OTanTalum <43750274+OTanTalum@users.noreply.github.com>
2019-12-05 01:42:35 +0200

    add header

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {StyleSheet, Image, View,Text} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export default class Main extends Component{
+
+  render() {
+    let pic = {
+      uri: 'https://i0.wp.com/apptractor.ru/wp-content/uploads/2018/04/1_jh6bmapyE8nPWju7W_7qEw.png?w=796&ssl=1'
+    };
+    return (
+        <View style={styles.background}>
+        <Image source={pic} style={{width: 450, height: 250}}/>
+        <Text style={styles.baseText}>Hello, I`m a Oleh</Text>
+        </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const styles=StyleSheet.create({
+    background:{
+        backgroundColor: '#1a1a39',
+        alignItems: 'center',
+    },
+    baseText:{
+        color: 'white',
+        fontFamily: 'monospace',
+        fontSize: 28,
+    }
 });
