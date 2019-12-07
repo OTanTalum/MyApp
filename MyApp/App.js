591c786ec28644f313ed6758f126fd0dd61187d3
OTanTalum <43750274+OTanTalum@users.noreply.github.com>
2019-12-07 14:30:53 +0200

    Add some style

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FlatList, ActivityIndicator, Text, View, StyleSheet  } from 'react-native';
+import {Image,FlatList, ActivityIndicator, Text, View, StyleSheet} from 'react-native';
 
 export default class SimpleApp extends React.Component {
     constructor(props){
@@ -13,8 +13,8 @@ export default class SimpleApp extends React.Component {
         let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
             "eyJpZCI6IjE5MTY5MzY2MjQxIiwiaWF0IjoxNTc0NzQ1MzkzfQ." +
             "KNjy849Hj6eJLSV_holfJzNGBU9UUoJADGb3Y4hBMj0";
-        return fetch('https://prod.mascotbe.com/groups/discover',{
-
+        return fetch('https://prod.mascotbe.com/groups/discover',
+            {
                 method: 'GET',
                 headers: {
                     Accept: 'application/json',
@@ -25,14 +25,10 @@ export default class SimpleApp extends React.Component {
         )
             .then((response) => response.json())
             .then((responseJson) => {
-
                 this.setState({
                     isLoading: false,
                     dataSource: responseJson.groups,
-                }, function(){
-
                 });
-
             })
             .catch((error) =>{
                 console.error(error);
@@ -44,19 +40,23 @@ export default class SimpleApp extends React.Component {
         if(this.state.isLoading){
             return(
                 <View style={styles.loading}>
-                    <ActivityIndicator style={styles.loading} size="large" color="#0000ff" />
+                    <ActivityIndicator size="large" color="#0000ff" />
                 </View>
             )
         }
         if(this.state.dataSource) {
             return(
-
-                <View style={styles.loading}>
+                <View style={styles.content}>
                     {
-
-                        <FlatList
+                        <FlatList style={styles.listOfItems}
                             data={this.state.dataSource}
-                            renderItem={({item}) => <Text>{item.name}</Text>}
+                            renderItem={({item}) =>
+                                <View style={styles.viewItemList}>
+                                    <Text style={styles.textItemListHead}>{item.name}</Text>
+                                    <Image style={styles.imageItemList} source={{uri: item.images[0].url}}/>
+                                    <Text style={styles.baseText}>{item.description}</Text>
+                                </View>
+                            }
                             keyExtractor={({id}, index) => id}
                         />
 
@@ -69,15 +69,51 @@ export default class SimpleApp extends React.Component {
 }
 
 
-
 const styles=StyleSheet.create({
     loading:{
         flex: 1,
         justifyContent: 'center',
     },
-    baseText:{
+    content:{
         flex: 1,
-        paddingTop:60,
-        paddingLeft:15,
+        backgroundColor: '#252d25',
+        paddingTop:35,
     },
+    baseText:{
+        flex: 2,
+        paddingTop:10,
+        paddingLeft:15,
+        fontSize:16,
+    },
+    listOfItems:{
+        backgroundColor:'lightgoldenrodyellow',
+        flexDirection: 'column',
+        paddingTop: 40,
+    },
+    viewItemList:{
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 5,
+        borderWidth: 1,
+        backgroundColor:'lightyellow',
+        borderColor: '#000',
+        elevation:4,
+        shadowOffset: { width: 5, height: 5 },
+        shadowColor: "black",
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+    },
+    imageItemList:{
+        marginVertical: 8,
+        alignSelf:'center',
+        resizeMode:'center',
+        width: 300,
+        height: 400,
+    },
+    textItemListHead:{
+        textAlign:'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+    }
 });
